6d6d1da426e54ad5b2995cbcca65a290f57ec38b
looktanme <wish._.hope@hotmail.com>
2016-03-12 14:54:37 +0700

    Create mul and div function

--- calculate.js
+++ calculate.js
@@ -4,5 +4,16 @@ function add(x,y){
 }
 
 function sub(x,y){
+	if(isNaN(x)) return console.log('Input must be number.');
 	return x - y;
+}
+
+function mul(x,y){
+	if(isNaN(x)) return console.log('Input must be number.');
+	return x * y;
+}
+
+function div(x,y){
+	if(isNaN(x)) return console.log('Input must be number.');
+	return x / y;
 }
